bff25c7765415630c73a14075165f0d3c21de75f
Henry Barreto <athenrybarreto@gmai.com>
2020-10-01 17:37:55 -0300

    Removing from git old folders

--- controllers/IController.ts
+++
@@ -1,6 +0,0 @@
-export default interface IController {
-  create(context: any): Promise<any>
-  read(context: any): Promise<any>
-  update(contex: any): Promise<any>
-  delete(context: any): Promise<any>
-}
--- controllers/IVotoController.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from "koa";
-
-export default interface IVotoController {
-  set(context: Context): Promise<any>
-  get(context: Context): Promise<any>
-}
